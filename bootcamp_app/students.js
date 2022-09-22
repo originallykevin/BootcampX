13e4f113f2679af82d5b56a5399995495f8a6fb2
originallykevin <ly1@ualberta.ca>
2022-09-22 04:30:51 +0000

    add npm postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const name = process.argv[2];
+const max = process.argv[3];
+
+pool.query(`
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE '%${name}%'
+  LIMIT ${max};
+  `)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  });
+
